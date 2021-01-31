ea5c14307adfe8e3bf88816fbc84a458c1f91d25
A Code Fox <heyacfox@gmail.com>
2021-01-30 19:36:00 -0500

    Background Music Up

---
+++ Assets/AShoeGame/Scripts/BackgroundMusicSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicSystem : MonoBehaviour
+{
+    [SerializeField] AudioSource mainLoopSource;
+    [SerializeField] AudioSource SongVox;
+    [SerializeField] AudioSource SongGuitar;
+    [SerializeField] bool vocalMusicInterrupt = false;
+    [SerializeField] float delayForFinalChorus = 180f;
+
+    public void Start()
+    {
+        SongVox.Play();
+        SongGuitar.Play();
+        mainLoopSource.PlayDelayed(SongVox.clip.length);
+        Invoke("QueueVocalsMusic", delayForFinalChorus);
+    }
+
+    public void QueueVocalsMusic()
+    {
+        mainLoopSource.loop = false;
+        vocalMusicInterrupt = true;
+    }
+
+    public void Update()
+    {
+        if (vocalMusicInterrupt)
+        {
+            if (!mainLoopSource.isPlaying)
+            {
+                SongVox.Play();
+                SongGuitar.Play();
+                mainLoopSource.PlayDelayed(SongVox.clip.length);
+                mainLoopSource.loop = true;
+                vocalMusicInterrupt = false;
+            }
+        }
+    }
+
+}
