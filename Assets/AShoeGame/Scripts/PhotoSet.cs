44d7cb2e93d24b6f012cd2c5f0632fb2987ef8ba
callenshaw <omegagames@gmail.com>
2021-01-31 15:56:52 -0500

    photos added to shelf

---
+++ Assets/AShoeGame/Scripts/PhotoSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoSet : MonoBehaviour
+{
+    public List<Texture2D> Photographs = new List<Texture2D>();
+
+    void Start()
+    {
+        if (Photographs.Count > 0)
+            foreach (var ps in GetComponentsInChildren<PhotoSwap>())
+            {
+                int ix = Random.Range(0, Photographs.Count);
+                ps.SetPicture(Photographs[ix]);
+                if (Photographs.Count > 1) Photographs.RemoveAt(ix);
+            }
+        
+    }
+}
---
+++ Assets/AShoeGame/Scripts/PhotoSwap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoSwap : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    public void SetPicture(Texture2D tex)
+    {
+        Renderer.material.mainTexture = tex;
+    }
+}
