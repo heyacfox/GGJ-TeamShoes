30b69e5eee56fc46f98a89f00ec825d6ffb52c4b
A Code Fox <heyacfox@gmail.com>
2021-01-31 12:05:54 -0500

    Made updates to how score looks

--- Assets/ScoreText.cs
+++ Assets/ScoreText.cs
@@ -13,8 +13,13 @@ public class ScoreText : MonoBehaviour
         if (!text)
             return;
         if (!NpcManager.Instance || NpcManager.Instance.Score == 0)
-            text.text = "";
+            text.text = "Pay: $0.00";
         else
-            text.text = string.Format(ScoreFormat, NpcManager.Instance.Score * 25);
+        {
+            int scoreToCents = NpcManager.Instance.Score * 25;
+            decimal moneyAmount = ((decimal)scoreToCents) / 100;
+            text.text = string.Format(ScoreFormat, moneyAmount);
+            //text.text = string.Format(ScoreFormat, NpcManager.Instance.Score * 25);
+        }
     }
 }
