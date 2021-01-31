178527ab5f046d0ae133b59101a1e9eacc68a373
callenshaw <omegagames@gmail.com>
2021-01-31 11:31:19 -0500

    hands now change pose when grab buttons pressed

---
+++ Assets/AShoeGame/Scripts/HandGripPoser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGripPoser : MonoBehaviour
+{
+    public bool IsLeft;
+    public GameObject ActiveWhenGripping;
+    public GameObject ActiveWhenNotGripping;
+
+    private void Start()
+    {
+        if (IsLeft) ActiveWhenGripping.transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void Update()
+    {
+        bool grip = false;
+        grip |= (IsLeft ? CallenVrWrapper.Inst.LeftGrip : CallenVrWrapper.Inst.RightGrip);
+        grip |= (IsLeft ? CallenVrWrapper.Inst.LeftTrigger : CallenVrWrapper.Inst.RightTrigger);
+        ActiveWhenGripping.SetActive(grip);
+        ActiveWhenNotGripping.SetActive(!grip);
+    }
+}
